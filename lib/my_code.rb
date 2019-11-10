c20ef399e82519ed99c7bf5e298c3cd280b3848d
Paul O <paul.orawiec@gmail.com>
2019-11-10 03:36:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,10 +52,12 @@ end
 def reduce_to_all_true(source_array)
   row_index = 0
   while row_index < source_array.length do
-
+    if !source_array[row_index]
+      return false
+    end
     row_index +=1
   end
-  
+  return true
 end
 
 def reduce_to_any_true(source_array)
