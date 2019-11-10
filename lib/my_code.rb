774f7d4e6ecfb556cb7467c4f99a42fc4da0506a
Paul O <paul.orawiec@gmail.com>
2019-11-10 03:07:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
 # My Code here....
+def map_to_negativize(source_array)
+  negative_array = []
+  row_index = 0
+  while row_index < source_array.length do
+    negative_array.push(source_array[row_index] * -1)
+    row_index += 1
+  end
+  source_array
+end
+
+def map_to_no_change(source_array)
+  
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
